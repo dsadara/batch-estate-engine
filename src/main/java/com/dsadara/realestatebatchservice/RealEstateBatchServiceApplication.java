3228b6fc19e9eed99c85e097736d8688e2756d7a
dsadara <chaehyun.forest@gmail.com>
2023-05-11 10:13:53 +0900

    first commit

---
+++ src/main/java/com/dsadara/realestatebatchservice/RealEstateBatchServiceApplication.java
@@ -0,0 +1,13 @@
+package com.dsadara.realestatebatchservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RealEstateBatchServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RealEstateBatchServiceApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/dsadara/realestatebatchservice/RealEstateBatchServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dsadara.realestatebatchservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RealEstateBatchServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
