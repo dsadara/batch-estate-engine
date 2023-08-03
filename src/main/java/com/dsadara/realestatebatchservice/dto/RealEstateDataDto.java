6ce7c36c895fb507e8c653f8ff9ec5e9b12261dc
dsadara <chaehyun.forest@gmail.com>
2023-08-03 13:14:48 +0900

    fix: 매핑되지 않는 필드 무시

--- src/main/java/com/dsadara/realestatebatchservice/dto/RealEstateDataDto.java
+++ src/main/java/com/dsadara/realestatebatchservice/dto/RealEstateDataDto.java
@@ -1,8 +1,9 @@
 package com.dsadara.realestatebatchservice.dto;
 
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import com.fasterxml.jackson.annotation.JsonProperty;
 
-
+@JsonIgnoreProperties(ignoreUnknown = true)
 public class RealEstateDataDto {
     // 공통
     @JsonProperty(value = "건축년도")
