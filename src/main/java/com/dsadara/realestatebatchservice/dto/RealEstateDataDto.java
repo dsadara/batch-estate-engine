4fe603c5d1f1f138f351bbf5933e1a6a7b5ec521
dsadara <chaehyun.forest@gmail.com>
2023-08-06 16:45:17 +0900

    test: 주택 종류별로 api 요청 테스트 fix: 주택 종류별 일치하지 않는 필드 JsonAlias 추가

--- src/main/java/com/dsadara/realestatebatchservice/dto/RealEstateDataDto.java
+++ src/main/java/com/dsadara/realestatebatchservice/dto/RealEstateDataDto.java
@@ -1,5 +1,6 @@
 package com.dsadara.realestatebatchservice.dto;
 
+import com.fasterxml.jackson.annotation.JsonAlias;
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import com.fasterxml.jackson.annotation.JsonProperty;
 import lombok.Getter;
@@ -9,27 +10,29 @@ import lombok.Getter;
 public class RealEstateDataDto {
     // 공통
     @JsonProperty(value = "건축년도")
-    private String constructYear;       // 건축년도
+    private String constructYear;
     @JsonProperty(value = "년")
-    private String contractYear;        // 년
+    private String contractYear;
     @JsonProperty(value = "아파트")
-    private String name;                // 이름
+    @JsonAlias({"연립다세대", "단지"})
+    private String name;
     @JsonProperty(value = "법정동")
-    private String legalDong;           // 법정동
+    private String legalDong;
     @JsonProperty(value = "시군구")
-    private String siGunGu;             // 시군구
+    private String siGunGu;
     @JsonProperty(value = "월")
-    private String month;               // 월
+    private String month;
     @JsonProperty(value = "일")
-    private String day;                 // 일
+    private String day;
     @JsonProperty(value = "전용면적")
-    private String jeonYongArea;        // 전용면적
+    @JsonAlias("계약면적")
+    private String jeonYongArea;
     @JsonProperty(value = "지번")
-    private String parcelNumber;        // 지번
+    private String parcelNumber;
     @JsonProperty(value = "지역코드")
-    private String regionCode;          // 지역코드
+    private String regionCode;
     @JsonProperty(value = "층")
-    private String floor;               // 층
+    private String floor;
     // 매매
     @JsonProperty(value = "거래금액")
     private String dealAmount;
@@ -39,21 +42,23 @@ public class RealEstateDataDto {
     private String CancelDealDay;
     @JsonProperty(value = "거래유형")
     private String dealType;
-    @JsonProperty(value = "중개업소주소")
+    @JsonProperty(value = "중개사소재지")
     private String agentAddress;
     // 전월세
     @JsonProperty(value = "갱신요구권사용")
-    private String requestRenewalRight; // 갱신요구권사용
+    private String requestRenewalRight;
     @JsonProperty(value = "계약구분")
-    private String contractType;        // 계약구분
+    private String contractType;
     @JsonProperty(value = "계약기간")
-    private String contractPeriod;      // 계약기간
+    private String contractPeriod;
     @JsonProperty(value = "월세금액")
-    private String monthlyRent;         // 월세금액
+    @JsonAlias("월세")
+    private String monthlyRent;
     @JsonProperty(value = "보증금액")
-    private String deposit;             // 보증금액
+    @JsonAlias("보증금")
+    private String deposit;
     @JsonProperty(value = "종전계약보증금")
-    private String depositBefore;       // 종전계약보증금
+    private String depositBefore;
     @JsonProperty(value = "종전계약월세")
-    private String monthlyRentBefore;   // 종전계약월세
+    private String monthlyRentBefore;
 }
---
+++ src/test/java/com/dsadara/realestatebatchservice/service/RequestDataRealTest.java
@@ -0,0 +1,184 @@
+package com.dsadara.realestatebatchservice.service;
+
+import com.dsadara.realestatebatchservice.dto.RealEstateDataDto;
+import org.junit.Test;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.assertNotEquals;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class RequestDataRealTest {
+
+    @Autowired
+    private RequestData requestData;
+
+
+    @Test
+    @DisplayName("성공-requestData()-아파트 전월세 api 요청")
+    public void requestData_Success_AptRent() throws Exception {
+        //given
+        String baseUrl = "http://openapi.molit.go.kr:8081/OpenAPI_ToolInstallPackage/service/rest/RTMSOBJSvc/getRTMSDataSvcAptRent";
+        String legalDongCode = "11200";
+        String contractYMD = "202206";
+        String serviceKey = "KNxUoxDnwzkyp3fb8dOjCWatfWm6VdGxJHzwOlvkSAcOcm%2B6%2BgIsOrcZ8Wr8hU0qzcmNE2tSjG7HUQBIA%2FqkYg%3D%3D";
+        //when
+        List<RealEstateDataDto> dtos = requestData.requestData(baseUrl, legalDongCode, contractYMD, serviceKey);
+
+        //then
+        assertNotNull(dtos);
+        assertNotEquals(0, dtos.size());
+        assertNotNull(dtos.get(0).getRequestRenewalRight());
+        assertNotNull(dtos.get(0).getConstructYear());
+        assertNotNull(dtos.get(0).getContractType());
+        assertNotNull(dtos.get(0).getContractPeriod());
+        assertNotNull(dtos.get(0).getContractYear());
+        assertNotNull(dtos.get(0).getLegalDong());
+        assertNotNull(dtos.get(0).getMonthlyRent());
+        assertNotNull(dtos.get(0).getName());
+        assertNotNull(dtos.get(0).getMonth());
+        assertNotNull(dtos.get(0).getDay());
+        assertNotNull(dtos.get(0).getJeonYongArea());
+        assertNotNull(dtos.get(0).getDeposit());
+        assertNotNull(dtos.get(0).getDepositBefore());
+        assertNotNull(dtos.get(0).getParcelNumber());
+        assertNotNull(dtos.get(0).getRegionCode());
+        assertNotNull(dtos.get(0).getFloor());
+        assertNotNull(dtos.get(0).getMonthlyRentBefore());
+    }
+
+    @Test
+    @DisplayName("성공-requestData()-아파트 매매 api 요청")
+    public void requestData_Success_AptTransaction() throws Exception {
+        //given
+        String baseUrl = "http://openapi.molit.go.kr:8081/OpenAPI_ToolInstallPackage/service/rest/RTMSOBJSvc/getRTMSDataSvcAptTrade";
+        String legalDongCode = "11200";
+        String contractYMD = "202007";
+        String serviceKey = "KNxUoxDnwzkyp3fb8dOjCWatfWm6VdGxJHzwOlvkSAcOcm%2B6%2BgIsOrcZ8Wr8hU0qzcmNE2tSjG7HUQBIA%2FqkYg%3D%3D";
+        //when
+        List<RealEstateDataDto> dtos = requestData.requestData(baseUrl, legalDongCode, contractYMD, serviceKey);
+
+        //then
+        assertNotNull(dtos);
+        assertNotNull(dtos.get(0));
+        assertNotEquals(0, dtos.size());
+        assertNotNull(dtos.get(0).getConstructYear());
+        assertNotNull(dtos.get(0).getContractYear());
+        assertNotNull(dtos.get(0).getLegalDong());
+        assertNotNull(dtos.get(0).getName());
+        assertNotNull(dtos.get(0).getMonth());
+        assertNotNull(dtos.get(0).getDay());
+        assertNotNull(dtos.get(0).getJeonYongArea());
+        assertNotNull(dtos.get(0).getParcelNumber());
+        assertNotNull(dtos.get(0).getRegionCode());
+        assertNotNull(dtos.get(0).getDealAmount());
+        assertNotNull(dtos.get(0).getCancelDealType());
+        assertNotNull(dtos.get(0).getCancelDealDay());
+        assertNotNull(dtos.get(0).getDealType());
+        assertNotNull(dtos.get(0).getAgentAddress());
+    }
+
+    @Test
+    @DisplayName("성공-requestData()-단독 다가구 api 요청")
+    public void requestData_Success_DetachedHouseRent() throws Exception {
+        //given
+        String baseUrl = "http://openapi.molit.go.kr:8081/OpenAPI_ToolInstallPackage/service/rest/RTMSOBJSvc/getRTMSDataSvcSHRent";
+        String legalDongCode = "11200";
+        String contractYMD = "202206";
+        String serviceKey = "KNxUoxDnwzkyp3fb8dOjCWatfWm6VdGxJHzwOlvkSAcOcm%2B6%2BgIsOrcZ8Wr8hU0qzcmNE2tSjG7HUQBIA%2FqkYg%3D%3D";
+        //when
+        List<RealEstateDataDto> dtos = requestData.requestData(baseUrl, legalDongCode, contractYMD, serviceKey);
+
+        //then
+        assertNotNull(dtos);
+        assertNotEquals(0, dtos.size());
+        assertNotNull(dtos.get(0).getRequestRenewalRight());
+        assertNotNull(dtos.get(0).getConstructYear());
+        assertNotNull(dtos.get(0).getContractType());
+        assertNotNull(dtos.get(0).getContractPeriod());
+        assertNotNull(dtos.get(0).getJeonYongArea());
+        assertNotNull(dtos.get(0).getContractYear());
+        assertNotNull(dtos.get(0).getLegalDong());
+        assertNotNull(dtos.get(0).getDeposit());
+        assertNotNull(dtos.get(0).getMonth());
+        assertNotNull(dtos.get(0).getMonthlyRent());
+        assertNotNull(dtos.get(0).getDay());
+        assertNotNull(dtos.get(0).getDepositBefore());
+        assertNotNull(dtos.get(0).getMonthlyRentBefore());
+        assertNotNull(dtos.get(0).getRegionCode());
+    }
+
+    @Test
+    @DisplayName("성공-requestData()- 연립 다세대 api 요청")
+    public void requestData_Success_RowHouseRent() throws Exception {
+        //given
+        String baseUrl = "http://openapi.molit.go.kr:8081/OpenAPI_ToolInstallPackage/service/rest/RTMSOBJSvc/getRTMSDataSvcRHRent";
+        String legalDongCode = "11200";
+        String contractYMD = "202206";
+        String serviceKey = "KNxUoxDnwzkyp3fb8dOjCWatfWm6VdGxJHzwOlvkSAcOcm%2B6%2BgIsOrcZ8Wr8hU0qzcmNE2tSjG7HUQBIA%2FqkYg%3D%3D";
+        //when
+        List<RealEstateDataDto> dtos = requestData.requestData(baseUrl, legalDongCode, contractYMD, serviceKey);
+
+        //then
+        assertNotNull(dtos);
+        assertNotEquals(0, dtos.size());
+        assertNotNull(dtos.get(0).getRequestRenewalRight());
+        assertNotNull(dtos.get(0).getConstructYear());
+        assertNotNull(dtos.get(0).getContractType());
+        assertNotNull(dtos.get(0).getContractPeriod());
+        assertNotNull(dtos.get(0).getContractYear());
+        assertNotNull(dtos.get(0).getLegalDong());
+        assertNotNull(dtos.get(0).getDeposit());
+        assertNotNull(dtos.get(0).getName());
+        assertNotNull(dtos.get(0).getMonth());
+        assertNotNull(dtos.get(0).getMonthlyRent());
+        assertNotNull(dtos.get(0).getDay());
+        assertNotNull(dtos.get(0).getJeonYongArea());
+        assertNotNull(dtos.get(0).getDepositBefore());
+        assertNotNull(dtos.get(0).getMonthlyRentBefore());
+        assertNotNull(dtos.get(0).getParcelNumber());
+        assertNotNull(dtos.get(0).getRegionCode());
+        assertNotNull(dtos.get(0).getFloor());
+    }
+
+    @Test
+    @DisplayName("성공-requestData()- 오피스텔 api 요청")
+    public void requestData_Success_EfficencyApt() throws Exception {
+        //given
+        String baseUrl = "http://openapi.molit.go.kr/OpenAPI_ToolInstallPackage/service/rest/RTMSOBJSvc/getRTMSDataSvcOffiRent";
+        String legalDongCode = "11200";
+        String contractYMD = "202206";
+        String serviceKey = "KNxUoxDnwzkyp3fb8dOjCWatfWm6VdGxJHzwOlvkSAcOcm%2B6%2BgIsOrcZ8Wr8hU0qzcmNE2tSjG7HUQBIA%2FqkYg%3D%3D";
+        //when
+        List<RealEstateDataDto> dtos = requestData.requestData(baseUrl, legalDongCode, contractYMD, serviceKey);
+
+        //then
+        assertNotNull(dtos);
+        assertNotEquals(0, dtos.size());
+        assertNotNull(dtos.get(0).getRequestRenewalRight());
+        assertNotNull(dtos.get(0).getConstructYear());
+        assertNotNull(dtos.get(0).getContractType());
+        assertNotNull(dtos.get(0).getContractPeriod());
+        assertNotNull(dtos.get(0).getContractYear());
+        assertNotNull(dtos.get(0).getName());
+        assertNotNull(dtos.get(0).getLegalDong());
+        assertNotNull(dtos.get(0).getDeposit());
+        assertNotNull(dtos.get(0).getSiGunGu());
+        assertNotNull(dtos.get(0).getMonth());
+        assertNotNull(dtos.get(0).getMonthlyRent());
+        assertNotNull(dtos.get(0).getDay());
+        assertNotNull(dtos.get(0).getJeonYongArea());
+        assertNotNull(dtos.get(0).getDepositBefore());
+        assertNotNull(dtos.get(0).getMonthlyRentBefore());
+        assertNotNull(dtos.get(0).getParcelNumber());
+        assertNotNull(dtos.get(0).getRegionCode());
+        assertNotNull(dtos.get(0).getFloor());
+    }
+}
