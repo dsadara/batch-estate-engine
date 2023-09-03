6c26c52a1c89c21b933be44fa8f2c942ffb013d3
dsadara <chaehyun.forest@gmail.com>
2023-09-04 00:05:44 +0900

    feat: jdbc Itemwriter로 쓰기속도 개선

--- src/main/java/com/dsadara/realestatebatchservice/job/CreateRealEstateJobConfig.java
+++ src/main/java/com/dsadara/realestatebatchservice/job/CreateRealEstateJobConfig.java
@@ -15,12 +15,15 @@ import org.springframework.batch.core.configuration.annotation.StepBuilderFactor
 import org.springframework.batch.core.configuration.annotation.StepScope;
 import org.springframework.batch.core.launch.support.RunIdIncrementer;
 import org.springframework.batch.item.ItemProcessor;
+import org.springframework.batch.item.ItemWriter;
 import org.springframework.batch.item.data.RepositoryItemWriter;
 import org.springframework.batch.item.data.builder.RepositoryItemWriterBuilder;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.web.client.HttpServerErrorException;
 
+import java.time.LocalDateTime;
 import java.util.concurrent.ExecutionException;
 
 @Configuration
@@ -30,6 +33,7 @@ public class CreateRealEstateJobConfig {
     private final JobBuilderFactory jobBuilderFactory;
     private final StepBuilderFactory stepBuilderFactory;
     private final RealEstateRepository realEstateRepository;
+    private final JdbcTemplate jdbcTemplate;
     private final RequestDataAsync requestDataAsync;
     private final GenerateApiQueryParam generateApiQueryParam;
 
@@ -45,10 +49,10 @@ public class CreateRealEstateJobConfig {
     @JobScope
     public Step createAptRentStep() {
         return stepBuilderFactory.get("createAptRentStep")
-                .<RealEstateDto, RealEstate>chunk(1000)
+                .<RealEstateDto, RealEstate>chunk(10000)
                 .reader(createAptRentReader())
                 .processor(createRealEstateProcessor())
-                .writer(createRealEstateWriter())
+                .writer(createJdbcRealEstateWriter())
                 .faultTolerant()
                 .skip(HttpServerErrorException.class)
                 .skip(ExecutionException.class)
@@ -67,31 +71,35 @@ public class CreateRealEstateJobConfig {
 
     @Bean
     public ItemProcessor<RealEstateDto, RealEstate> createRealEstateProcessor() {
-        return RealEstateDto -> RealEstate.builder()
-                .constructYear(RealEstateDto.getConstructYear())
-                .contractYear(RealEstateDto.getContractYear())
-                .name(RealEstateDto.getName())
-                .legalDong(RealEstateDto.getLegalDong())
-                .siGunGu(RealEstateDto.getSiGunGu())
-                .month(RealEstateDto.getMonth())
-                .day(RealEstateDto.getDay())
-                .jeonYongArea(RealEstateDto.getJeonYongArea())
-                .parcelNumber(RealEstateDto.getParcelNumber())
-                .regionCode(RealEstateDto.getRegionCode())
-                .floor(RealEstateDto.getFloor())
-                .dealAmount(RealEstateDto.getDealAmount())
-                .CancelDealType(RealEstateDto.getCancelDealType())
-                .CancelDealDay(RealEstateDto.getCancelDealDay())
-                .dealType(RealEstateDto.getDealType())
-                .agentAddress(RealEstateDto.getAgentAddress())
-                .requestRenewalRight(RealEstateDto.getRequestRenewalRight())
-                .contractType(RealEstateDto.getContractType())
-                .contractPeriod(RealEstateDto.getContractPeriod())
-                .monthlyRent(RealEstateDto.getMonthlyRent())
-                .deposit(RealEstateDto.getDeposit())
-                .depositBefore(RealEstateDto.getDepositBefore())
-                .monthlyRentBefore(RealEstateDto.getMonthlyRentBefore())
-                .build();
+        return RealEstateDto -> {
+            LocalDateTime now = LocalDateTime.now();
+            return RealEstate.builder()
+                    .constructYear(RealEstateDto.getConstructYear())
+                    .contractYear(RealEstateDto.getContractYear())
+                    .name(RealEstateDto.getName())
+                    .legalDong(RealEstateDto.getLegalDong())
+                    .siGunGu(RealEstateDto.getSiGunGu())
+                    .month(RealEstateDto.getMonth())
+                    .day(RealEstateDto.getDay())
+                    .jeonYongArea(RealEstateDto.getJeonYongArea())
+                    .parcelNumber(RealEstateDto.getParcelNumber())
+                    .regionCode(RealEstateDto.getRegionCode())
+                    .floor(RealEstateDto.getFloor())
+                    .dealAmount(RealEstateDto.getDealAmount())
+                    .CancelDealType(RealEstateDto.getCancelDealType())
+                    .CancelDealDay(RealEstateDto.getCancelDealDay())
+                    .dealType(RealEstateDto.getDealType())
+                    .agentAddress(RealEstateDto.getAgentAddress())
+                    .requestRenewalRight(RealEstateDto.getRequestRenewalRight())
+                    .contractType(RealEstateDto.getContractType())
+                    .contractPeriod(RealEstateDto.getContractPeriod())
+                    .monthlyRent(RealEstateDto.getMonthlyRent())
+                    .deposit(RealEstateDto.getDeposit())
+                    .depositBefore(RealEstateDto.getDepositBefore())
+                    .monthlyRentBefore(RealEstateDto.getMonthlyRentBefore())
+                    .createdAt(now)
+                    .build();
+        };
     }
 
     @Bean
@@ -99,5 +107,63 @@ public class CreateRealEstateJobConfig {
         return new RepositoryItemWriterBuilder<RealEstate>()
                 .repository(realEstateRepository)
                 .build();
+    }
+
+    @Bean
+    public ItemWriter<RealEstate> createJdbcRealEstateWriter() {
+        return items -> jdbcTemplate.batchUpdate("insert into REAL_ESTATE (" +
+                        "cancel_deal_day," +
+                        "cancel_deal_type," +
+                        "agent_address," +
+                        "construct_year," +
+                        "contract_period," +
+                        "contract_type," +
+                        "contract_year," +
+                        "created_at," +
+                        "day," +
+                        "deal_amount," +
+                        "deal_type," +
+                        "deposit," +
+                        "deposit_before," +
+                        "floor," +
+                        "jeon_yong_area," +
+                        "legal_dong," +
+                        "month," +
+                        "monthly_rent," +
+                        "monthly_rent_before," +
+                        "name," +
+                        "parcel_number," +
+                        "region_code," +
+                        "request_renewal_right," +
+                        "si_gun_gu) values (" +
+                        "?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)",
+                items,
+                10000,
+                (ps, RealEstate) -> {
+                    ps.setObject(1, RealEstate.getCancelDealDay());
+                    ps.setObject(2, RealEstate.getCancelDealType());
+                    ps.setObject(3, RealEstate.getAgentAddress());
+                    ps.setObject(4, RealEstate.getConstructYear());
+                    ps.setObject(5, RealEstate.getContractPeriod());
+                    ps.setObject(6, RealEstate.getContractType());
+                    ps.setObject(7, RealEstate.getContractYear());
+                    ps.setObject(8, RealEstate.getCreatedAt());
+                    ps.setObject(9, RealEstate.getDay());
+                    ps.setObject(10, RealEstate.getDealAmount());
+                    ps.setObject(11, RealEstate.getDealType());
+                    ps.setObject(12, RealEstate.getDeposit());
+                    ps.setObject(13, RealEstate.getDepositBefore());
+                    ps.setObject(14, RealEstate.getFloor());
+                    ps.setObject(15, RealEstate.getJeonYongArea());
+                    ps.setObject(16, RealEstate.getLegalDong());
+                    ps.setObject(17, RealEstate.getMonth());
+                    ps.setObject(18, RealEstate.getMonthlyRent());
+                    ps.setObject(19, RealEstate.getMonthlyRentBefore());
+                    ps.setObject(20, RealEstate.getName());
+                    ps.setObject(21, RealEstate.getParcelNumber());
+                    ps.setObject(22, RealEstate.getRegionCode());
+                    ps.setObject(23, RealEstate.getRequestRenewalRight());
+                    ps.setObject(24, RealEstate.getSiGunGu());
+                });
     }
 }
