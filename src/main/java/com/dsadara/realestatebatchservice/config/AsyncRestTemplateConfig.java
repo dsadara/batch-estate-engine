1e05ccd8e783a82b4e86a2f1e5590cb05fd5a7be
dsadara <chaehyun.forest@gmail.com>
2024-01-16 23:59:56 +0900

    fix: netty 제거
    추후 비동기 호출을 구현할 시 WebClient를 사용

--- src/main/java/com/dsadara/realestatebatchservice/config/AsyncRestTemplateConfig.java
+++ src/main/java/com/dsadara/realestatebatchservice/config/AsyncRestTemplateConfig.java
@@ -1,9 +1,7 @@
 package com.dsadara.realestatebatchservice.config;
 
-import io.netty.channel.nio.NioEventLoopGroup;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.http.client.Netty4ClientHttpRequestFactory;
 import org.springframework.web.client.AsyncRestTemplate;
 
 @Configuration
@@ -11,9 +9,7 @@ public class AsyncRestTemplateConfig {
 
     @Bean
     public AsyncRestTemplate restTemplateAsync() {
-        Netty4ClientHttpRequestFactory factory =
-                new Netty4ClientHttpRequestFactory(new NioEventLoopGroup(1));
-        return new AsyncRestTemplate(factory);
+        return new AsyncRestTemplate();
     }
 
 }
