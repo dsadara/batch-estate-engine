7a160d301b1877f64bbdf8a229777b84f9f97ff9
dsadara <chaehyun.forest@gmail.com>
2024-01-22 23:43:21 +0900

    refactor: 파라미터만 다른 중복 기능 메소드 제거

--- src/main/java/com/dsadara/realestatebatchservice/service/RequestData.java
+++ src/main/java/com/dsadara/realestatebatchservice/service/RequestData.java
@@ -9,7 +9,6 @@ import org.springframework.util.LinkedMultiValueMap;
 import org.springframework.util.MultiValueMap;
 
 import java.util.ArrayList;
-import java.util.LinkedList;
 import java.util.List;
 import java.util.Optional;
 
@@ -18,11 +17,9 @@ import java.util.Optional;
 public class RequestData {
 
     private final JsonDeserializer jsonDeserializer;
-    private final GenerateApiQueryParam generateApiQueryParam;
 
-    public RequestData(JsonDeserializer jsonDeserializer, GenerateApiQueryParam generateApiQueryParam) {
+    public RequestData(JsonDeserializer jsonDeserializer) {
         this.jsonDeserializer = jsonDeserializer;
-        this.generateApiQueryParam = generateApiQueryParam;
     }
 
     public List<RealEstateDto> requestData(String baseURL, String servicekey, String bjdCode, String contractYMD) throws Exception {
@@ -38,103 +35,4 @@ public class RequestData {
 
         return realEstateDtos;
     }
-
-    public List<RealEstateDto> requestOneLegalDongData(String baseURL, String legalDongCode, String servicekey) throws Exception {
-        List<RealEstateDto> result = new ArrayList<>();
-        for (String dealYearMonth : generateApiQueryParam.getDealYearMonthsList()) {
-            result.addAll(requestData(baseURL, legalDongCode, dealYearMonth, servicekey));
-        }
-        return result;
-    }
-
-    public List<RealEstateDto> requestAptRentData() throws Exception {
-        List<String> bjdCodeList = generateApiQueryParam.getBjdCodeList();
-        List<String> dealYearMonthsList = generateApiQueryParam.getDealYearMonthsList();
-        List<RealEstateDto> result = new LinkedList<>();
-
-        String baseUrl = "http://openapi.molit.go.kr:8081/OpenAPI_ToolInstallPackage/service/rest/RTMSOBJSvc/getRTMSDataSvcAptRent";
-        String serviceKey = "KNxUoxDnwzkyp3fb8dOjCWatfWm6VdGxJHzwOlvkSAcOcm%2B6%2BgIsOrcZ8Wr8hU0qzcmNE2tSjG7HUQBIA%2FqkYg%3D%3D";
-
-        for (String bjdCode : bjdCodeList) {
-            log.info(String.format("[아파트 전월세][데이터 요청][법정동코드:%s]", bjdCode));
-            for (String dealYearMonth : dealYearMonthsList) {
-                result.addAll(requestData(baseUrl, bjdCode, dealYearMonth, serviceKey));
-            }
-        }
-
-        return result;
-    }
-
-    public List<RealEstateDto> requestAptTradeData() throws Exception {
-        List<String> bjdCodeList = generateApiQueryParam.getBjdCodeList();
-        List<String> dealYearMonthsList = generateApiQueryParam.getDealYearMonthsList();
-        List<RealEstateDto> result = new LinkedList<>();
-
-        String baseUrl = "http://openapi.molit.go.kr:8081/OpenAPI_ToolInstallPackage/service/rest/RTMSOBJSvc/getRTMSDataSvcAptTrade";
-        String serviceKey = "KNxUoxDnwzkyp3fb8dOjCWatfWm6VdGxJHzwOlvkSAcOcm%2B6%2BgIsOrcZ8Wr8hU0qzcmNE2tSjG7HUQBIA%2FqkYg%3D%3D";
-
-        for (String bjdCode : bjdCodeList) {
-            log.info(String.format("[아파트 매매][데이터 요청][법정동코드:%s]", bjdCode));
-            for (String dealYearMonth : dealYearMonthsList) {
-                result.addAll(requestData(baseUrl, bjdCode, dealYearMonth, serviceKey));
-            }
-        }
-
-        return result;
-    }
-
-    public List<RealEstateDto> requestRowHouseRent() throws Exception {
-        List<String> bjdCodeList = generateApiQueryParam.getBjdCodeList();
-        List<String> dealYearMonthsList = generateApiQueryParam.getDealYearMonthsList();
-        List<RealEstateDto> result = new LinkedList<>();
-
-        String baseUrl = "http://openapi.molit.go.kr:8081/OpenAPI_ToolInstallPackage/service/rest/RTMSOBJSvc/getRTMSDataSvcRHRent";
-        String serviceKey = "KNxUoxDnwzkyp3fb8dOjCWatfWm6VdGxJHzwOlvkSAcOcm%2B6%2BgIsOrcZ8Wr8hU0qzcmNE2tSjG7HUQBIA%2FqkYg%3D%3D";
-
-        for (String bjdCode : bjdCodeList) {
-            log.info(String.format("[연립 다세대][데이터 요청][법정동코드:%s]", bjdCode));
-            for (String dealYearMonth : dealYearMonthsList) {
-                result.addAll(requestData(baseUrl, bjdCode, dealYearMonth, serviceKey));
-            }
-        }
-
-        return result;
-    }
-
-    public List<RealEstateDto> requestEfficencyAptRent() throws Exception {
-        List<String> bjdCodeList = generateApiQueryParam.getBjdCodeList();
-        List<String> dealYearMonthsList = generateApiQueryParam.getDealYearMonthsList();
-        List<RealEstateDto> result = new LinkedList<>();
-
-        String baseUrl = "http://openapi.molit.go.kr/OpenAPI_ToolInstallPackage/service/rest/RTMSOBJSvc/getRTMSDataSvcOffiRent";
-        String serviceKey = "KNxUoxDnwzkyp3fb8dOjCWatfWm6VdGxJHzwOlvkSAcOcm%2B6%2BgIsOrcZ8Wr8hU0qzcmNE2tSjG7HUQBIA%2FqkYg%3D%3D";
-
-        for (String bjdCode : bjdCodeList) {
-            log.info(String.format("[오피스텔][데이터 요청][법정동코드:%s]", bjdCode));
-            for (String dealYearMonth : dealYearMonthsList) {
-                result.addAll(requestData(baseUrl, bjdCode, dealYearMonth, serviceKey));
-            }
-        }
-
-        return result;
-    }
-
-    public List<RealEstateDto> requestDetachedHouseRent() throws Exception {
-        List<String> bjdCodeList = generateApiQueryParam.getBjdCodeList();
-        List<String> dealYearMonthsList = generateApiQueryParam.getDealYearMonthsList();
-        List<RealEstateDto> result = new LinkedList<>();
-
-        String baseUrl = "http://openapi.molit.go.kr:8081/OpenAPI_ToolInstallPackage/service/rest/RTMSOBJSvc/getRTMSDataSvcSHRent";
-        String serviceKey = "KNxUoxDnwzkyp3fb8dOjCWatfWm6VdGxJHzwOlvkSAcOcm%2B6%2BgIsOrcZ8Wr8hU0qzcmNE2tSjG7HUQBIA%2FqkYg%3D%3D";
-
-        for (String bjdCode : bjdCodeList) {
-            log.info(String.format("[단독 다가구][데이터 요청][법정동코드:%s]", bjdCode));
-            for (String dealYearMonth : dealYearMonthsList) {
-                result.addAll(requestData(baseUrl, bjdCode, dealYearMonth, serviceKey));
-            }
-        }
-
-        return result;
-    }
-
 }
