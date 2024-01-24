696a168053c8755bf3fb0124affd08992758516d
dsadara <chaehyun.forest@gmail.com>
2024-01-24 23:21:11 +0900

    feat: step 실행 중 발생한 에러 로깅

--- src/main/java/com/dsadara/realestatebatchservice/job/StepExceptionLogger.java
+++ src/main/java/com/dsadara/realestatebatchservice/job/StepExceptionLogger.java
@@ -1,18 +1,30 @@
 package com.dsadara.realestatebatchservice.job;
 
+import lombok.extern.slf4j.Slf4j;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.batch.core.BatchStatus;
 import org.springframework.batch.core.ExitStatus;
 import org.springframework.batch.core.StepExecution;
 import org.springframework.batch.core.StepExecutionListener;
 
+
+@Slf4j
 public class StepExceptionLogger implements StepExecutionListener {
+    private final Logger logger = LoggerFactory.getLogger(this.getClass());
+
     @Override
     public void beforeStep(StepExecution stepExecution) {
-        System.out.println("Step 시작 전: " + stepExecution.getStepName());
+
     }
 
     @Override
     public ExitStatus afterStep(StepExecution stepExecution) {
-        System.out.println("Step 종료 후: " + stepExecution.getStepName());
+        if (stepExecution.getStatus() == BatchStatus.FAILED) {
+            Throwable exception = stepExecution.getFailureExceptions().get(0);
+            logger.error("[Step 에러 발생][법정동 코드 {}][계약 연월일 {}] ErrorMessage : {}",
+                    "11110", "201501", exception.getMessage());
+        }
         return stepExecution.getExitStatus();
     }
 }
