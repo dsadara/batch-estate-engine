c2796a4b8d855f821a045429a2dace7cd53c476d
dsadara <chaehyun.forest@gmail.com>
2024-01-25 23:21:11 +0900

    refactor: step의 다중 예외 처리 가능하게 변경

--- src/main/java/com/dsadara/realestatebatchservice/job/StepExceptionLogger.java
+++ src/main/java/com/dsadara/realestatebatchservice/job/StepExceptionLogger.java
@@ -3,10 +3,11 @@ package com.dsadara.realestatebatchservice.job;
 import lombok.extern.slf4j.Slf4j;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.springframework.batch.core.BatchStatus;
 import org.springframework.batch.core.ExitStatus;
 import org.springframework.batch.core.StepExecution;
 import org.springframework.batch.core.StepExecutionListener;
+
+import java.util.List;
 
 
 @Slf4j
@@ -20,11 +21,15 @@ public class StepExceptionLogger implements StepExecutionListener {
 
     @Override
     public ExitStatus afterStep(StepExecution stepExecution) {
-        if (stepExecution.getStatus() == BatchStatus.FAILED) {
-            Throwable exception = stepExecution.getFailureExceptions().get(0);
-            logger.error("[Step 에러 발생][법정동 코드 {}][계약 연월일 {}] ErrorMessage : {}",
-                    "11110", "201501", exception.getMessage());
+        List<Throwable> failureExceptions = stepExecution.getFailureExceptions();
+
+        if (!failureExceptions.isEmpty()) {
+            for (Throwable failureException : failureExceptions) {
+                logger.error("[Step 에러 발생][법정동 코드 {}][계약 연월일 {}] ErrorMessage : {}",
+                        "11110", "201501", failureException.getMessage());
+            }
         }
+
         return stepExecution.getExitStatus();
     }
 }
