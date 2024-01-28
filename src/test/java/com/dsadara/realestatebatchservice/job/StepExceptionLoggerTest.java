e2be3e2c7fbb5a2970ef89a40b701a84479e807b
dsadara <chaehyun.forest@gmail.com>
2024-01-28 23:21:12 +0900

    test: step 에러 로깅 검증

--- src/test/java/com/dsadara/realestatebatchservice/job/StepExceptionLoggerTest.java
+++ src/test/java/com/dsadara/realestatebatchservice/job/StepExceptionLoggerTest.java
@@ -1,10 +1,14 @@
 package com.dsadara.realestatebatchservice.job;
 
+import ch.qos.logback.classic.Logger;
+import ch.qos.logback.classic.spi.ILoggingEvent;
+import ch.qos.logback.core.read.ListAppender;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.junit.jupiter.MockitoExtension;
+import org.slf4j.LoggerFactory;
 import org.springframework.batch.core.Job;
 import org.springframework.batch.core.JobExecution;
 import org.springframework.batch.core.JobParameters;
@@ -22,6 +26,7 @@ import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
 
+import static org.assertj.core.api.Assertions.assertThat;
 import static org.mockito.Mockito.when;
 
 @SpringBatchTest
@@ -40,11 +45,11 @@ class StepExceptionLoggerTest {
     @MockBean
     private ItemReader<String> itemReader;
     private Job testJob;
+    private ListAppender<ILoggingEvent> listAppender;
 
     @BeforeEach
     void setUpJob() throws Exception {
         when(itemReader.read()).thenThrow(new RuntimeException("Test Exception"));
-
         StepExceptionLogger stepExceptionLogger = new StepExceptionLogger();
 
         Step testStep = stepBuilderFactory.get("testStep")
@@ -60,6 +65,14 @@ class StepExceptionLoggerTest {
                 .build();
     }
 
+    @BeforeEach
+    void setUpLogger() {
+        Logger logger = (Logger) LoggerFactory.getLogger(StepExceptionLogger.class);
+        listAppender = new ListAppender<>();
+        listAppender.start();
+        logger.addAppender(listAppender);
+    }
+
     @Test
     public void testStepFailure() throws Exception {
         // given, when
@@ -72,5 +85,18 @@ class StepExceptionLoggerTest {
         // then
         Assertions.assertFalse(stepExecution.getFailureExceptions().isEmpty());
         Assertions.assertTrue(stepExecution.getFailureExceptions().get(0).getMessage().contains("Test Exception"));
+    }
+
+    @Test
+    public void testStepFailureLogging() throws Exception {
+        // given, when
+        SimpleJobLauncher testJobLauncher = new SimpleJobLauncher();
+        testJobLauncher.setJobRepository(jobRepository);
+        testJobLauncher.afterPropertiesSet();
+        testJobLauncher.run(testJob, new JobParameters());
+
+        // then
+        assertThat(listAppender.list).extracting(ILoggingEvent::getMessage)
+                .contains("[Step 에러 발생][법정동 코드 {}][계약 연월일 {}] ErrorMessage : {}");
     }
 }
