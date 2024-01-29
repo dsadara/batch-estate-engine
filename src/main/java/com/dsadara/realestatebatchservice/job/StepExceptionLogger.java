2e62b73f85a50dd6d15e2559e8a35c20651e2e4b
dsadara <chaehyun.forest@gmail.com>
2024-01-29 23:21:12 +0900

    fix: lombok이 생성하는 logger를 사용

--- src/main/java/com/dsadara/realestatebatchservice/job/StepExceptionLogger.java
+++ src/main/java/com/dsadara/realestatebatchservice/job/StepExceptionLogger.java
@@ -1,8 +1,6 @@
 package com.dsadara.realestatebatchservice.job;
 
 import lombok.extern.slf4j.Slf4j;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 import org.springframework.batch.core.ExitStatus;
 import org.springframework.batch.core.StepExecution;
 import org.springframework.batch.core.StepExecutionListener;
@@ -12,7 +10,6 @@ import java.util.List;
 
 @Slf4j
 public class StepExceptionLogger implements StepExecutionListener {
-    private final Logger logger = LoggerFactory.getLogger(this.getClass());
 
     @Override
     public void beforeStep(StepExecution stepExecution) {
@@ -25,7 +22,7 @@ public class StepExceptionLogger implements StepExecutionListener {
 
         if (!failureExceptions.isEmpty()) {
             for (Throwable failureException : failureExceptions) {
-                logger.error("[Step 에러 발생][법정동 코드 {}][계약 연월일 {}] ErrorMessage : {}",
+                log.error("[Step 에러 발생][법정동 코드 {}][계약 연월일 {}] ErrorMessage : {}",
                         "11110", "201501", failureException.getMessage());
             }
         }
