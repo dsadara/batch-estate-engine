e81c5d0f3938222cb5f4ef370cecba7cd867fe37
dsadara <chaehyun.forest@gmail.com>
2024-02-19 23:21:12 +0900

    feat: 모든 법정동에 대한 job을 실행

--- src/main/java/com/dsadara/realestatebatchservice/runner/RealEstateJobLauncherCommandLineRunner.java
+++ src/main/java/com/dsadara/realestatebatchservice/runner/RealEstateJobLauncherCommandLineRunner.java
@@ -1,6 +1,7 @@
 package com.dsadara.realestatebatchservice.runner;
 
 import com.dsadara.realestatebatchservice.launcher.RealEstateJobLauncher;
+import com.dsadara.realestatebatchservice.service.GenerateApiQueryParam;
 import org.springframework.batch.core.JobParameters;
 import org.springframework.batch.core.JobParametersBuilder;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -11,6 +12,8 @@ public class RealEstateJobLauncherCommandLineRunner implements CommandLineRunner
 
     @Autowired
     private RealEstateJobLauncher realEstateJobLauncher;
+    @Autowired
+    private GenerateApiQueryParam generateApiQueryParam;
     @Value("${openapi.request.url.aptRent}")
     private String baseUrl;
     @Value("${openapi.request.serviceKey}")
@@ -18,13 +21,15 @@ public class RealEstateJobLauncherCommandLineRunner implements CommandLineRunner
 
     @Override
     public void run(String... args) throws Exception {
-        JobParameters parameters = new JobParametersBuilder()
-                .addString("baseUrl", baseUrl)
-                .addString("serviceKey", serviceKey)
-                .addString("bjdCode", "11110")
-                .addLong("time", System.currentTimeMillis())
-                .toJobParameters();
-        realEstateJobLauncher.launchJob(parameters);
+        for (String bjdCode : generateApiQueryParam.getBjdCodeList()) {
+            JobParameters parameters = new JobParametersBuilder()
+                    .addString("baseUrl", baseUrl)
+                    .addString("serviceKey", serviceKey)
+                    .addString("bjdCode", bjdCode)
+                    .addLong("time", System.currentTimeMillis())
+                    .toJobParameters();
+            realEstateJobLauncher.launchJob(parameters);
+        }
     }
 
 }
