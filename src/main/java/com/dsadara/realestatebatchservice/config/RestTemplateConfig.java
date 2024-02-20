ff7cf5ccf7a52a0ce70b0513026165dc4844cbf7
dsadara <chaehyun.forest@gmail.com>
2024-02-20 23:21:12 +0900

    refactor: RestTemplate 필요없는 설정 제거

--- src/main/java/com/dsadara/realestatebatchservice/config/RestTemplateConfig.java
+++ src/main/java/com/dsadara/realestatebatchservice/config/RestTemplateConfig.java
@@ -1,10 +1,7 @@
 package com.dsadara.realestatebatchservice.config;
 
-import org.apache.http.client.HttpClient;
-import org.apache.http.impl.client.HttpClientBuilder;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.http.client.HttpComponentsClientHttpRequestFactory;
 import org.springframework.web.client.RestTemplate;
 import org.springframework.web.util.DefaultUriBuilderFactory;
 
@@ -13,16 +10,7 @@ public class RestTemplateConfig {
 
     @Bean
     public RestTemplate restTemplate() {
-        HttpComponentsClientHttpRequestFactory factory = new HttpComponentsClientHttpRequestFactory();
-        factory.setConnectionRequestTimeout(5000 * 1000);   // 커넥션풀에서 사용 가능한 연결을 가져오기 위해 대기하는 최대 시간
-        factory.setConnectTimeout(5000);                    // 커넥션 최대 시간
-        factory.setReadTimeout(5000);                       // 읽기 최대 시간
-        HttpClient httpClient = HttpClientBuilder.create()
-                .setMaxConnTotal(100)
-                .setMaxConnPerRoute(60)
-                .build();
-        factory.setHttpClient(httpClient);
-        RestTemplate restTemplate = new RestTemplate(factory);
+        RestTemplate restTemplate = new RestTemplate();
         DefaultUriBuilderFactory defaultUriBuilderFactory = new DefaultUriBuilderFactory();
         defaultUriBuilderFactory.setEncodingMode(DefaultUriBuilderFactory.EncodingMode.NONE);
         restTemplate.setUriTemplateHandler(defaultUriBuilderFactory);
