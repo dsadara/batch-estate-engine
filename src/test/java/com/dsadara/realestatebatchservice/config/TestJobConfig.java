bb6fe8ae249b35481261f122b46d41cc947531f9
dsadara <chaehyun.forest@gmail.com>
2024-02-28 23:21:12 +0900

    test: 테스트용 job configuration 생성
    RealEstateJob과 동일한 구조이지만 api 호출로 인한 레이턴시가 없음

---
+++ src/test/java/com/dsadara/realestatebatchservice/config/TestJobConfig.java
@@ -0,0 +1,106 @@
+package com.dsadara.realestatebatchservice.config;
+
+import com.dsadara.realestatebatchservice.domain.RealEstate;
+import com.dsadara.realestatebatchservice.dto.RealEstateDto;
+import com.dsadara.realestatebatchservice.listener.StepExceptionLogger;
+import com.dsadara.realestatebatchservice.service.GenerateApiQueryParam;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.batch.core.Job;
+import org.springframework.batch.core.Step;
+import org.springframework.batch.core.configuration.annotation.JobBuilderFactory;
+import org.springframework.batch.core.configuration.annotation.JobScope;
+import org.springframework.batch.core.configuration.annotation.StepBuilderFactory;
+import org.springframework.batch.core.configuration.annotation.StepScope;
+import org.springframework.batch.core.launch.support.RunIdIncrementer;
+import org.springframework.batch.core.partition.support.Partitioner;
+import org.springframework.batch.item.ExecutionContext;
+import org.springframework.batch.item.ItemProcessor;
+import org.springframework.batch.item.ItemReader;
+import org.springframework.batch.item.ItemWriter;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+@Slf4j
+@RequiredArgsConstructor
+@Configuration
+public class TestJobConfig {
+
+    private final JobBuilderFactory jobBuilderFactory;
+    private final StepBuilderFactory stepBuilderFactory;
+    private final GenerateApiQueryParam generateApiQueryParam;
+    private final StepExceptionLogger stepExceptionLogger;
+
+    @Bean
+    public Job TestJob() throws Exception {
+        return jobBuilderFactory.get("testJob")
+                .incrementer(new RunIdIncrementer())
+                .flow(testMasterStep(null))
+                .end()
+                .build();
+    }
+
+    @Bean
+    @JobScope
+    public Step testMasterStep(@Value("#{jobParameters['bjdCode']}") String bjdCode) throws Exception {
+        return stepBuilderFactory.get("법정동코드" + ":" + bjdCode)
+                .partitioner("slaveStep", testDealYearMonthPartitioner())
+                .step(testSlaveStep())
+                .gridSize(300)
+                .build();
+    }
+
+    @Bean
+    @JobScope
+    public Step testSlaveStep() throws Exception {
+        return stepBuilderFactory.get("계약월")
+                .<RealEstateDto, RealEstate>chunk(100)
+                .reader(createEmptyItemReader(null, null))
+                .processor(createEmptyProcessor())
+                .writer(createEmptyWriter())
+                .faultTolerant()
+                .skipLimit(50)
+                .skip(Exception.class)
+                .listener(stepExceptionLogger)
+                .build();
+    }
+
+    @Bean
+    @StepScope
+    public ItemReader<RealEstateDto> createEmptyItemReader(
+            @Value("#{jobParameters['baseUrl']}") String baseUrl,
+            @Value("#{jobParameters['serviceKey']}") String serviceKey) throws Exception {
+        return RealEstateDto::new;
+    }
+
+    @Bean
+    public ItemProcessor<RealEstateDto, RealEstate> createEmptyProcessor() {
+        return RealEstateDto -> new RealEstate();
+    }
+
+    @Bean
+    public ItemWriter<RealEstate> createEmptyWriter() {
+        return items -> {
+        };
+    }
+
+    @Bean
+    public Partitioner testDealYearMonthPartitioner() {
+        return gridSize -> {
+            Map<String, ExecutionContext> result = new HashMap<>();
+            List<String> dealYearMonthList = generateApiQueryParam.getDealYearMonthsList();
+            for (String dealYearMonth : dealYearMonthList) {
+                ExecutionContext executionContext = new ExecutionContext();
+                executionContext.putString("dealYearMonth", dealYearMonth);
+                result.put(dealYearMonth, executionContext);
+            }
+            return result;
+        };
+    }
+
+}
