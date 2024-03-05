558e388f8c6bbae3e45ee6071a0f35453aa55e0c
dsadara <chaehyun.forest@gmail.com>
2024-03-05 23:21:12 +0900

    refactor: Job 인터페이스에 여러 구현체를 주입 받을 수 있도록 변경

--- src/main/java/com/dsadara/realestatebatchservice/launcher/RealEstateJobLauncher.java
+++ src/main/java/com/dsadara/realestatebatchservice/launcher/RealEstateJobLauncher.java
@@ -1,23 +1,21 @@
 package com.dsadara.realestatebatchservice.launcher;
 
-import com.dsadara.realestatebatchservice.config.RealEstateJobConfig;
 import lombok.RequiredArgsConstructor;
+import org.springframework.batch.core.Job;
 import org.springframework.batch.core.JobExecution;
 import org.springframework.batch.core.JobParameters;
 import org.springframework.batch.core.launch.JobLauncher;
-import org.springframework.context.annotation.Profile;
 import org.springframework.stereotype.Component;
 
-@Profile("!test")
 @RequiredArgsConstructor
 @Component
 public class RealEstateJobLauncher {
 
     private final JobLauncher jobLauncher;
-    private final RealEstateJobConfig realEstateJobConfig;
+    private final Job realEstateJob;
 
     public JobExecution launchJob(JobParameters parameters) throws Exception {
-        return jobLauncher.run(realEstateJobConfig.createRealEstateJob(), parameters);
+        return jobLauncher.run(realEstateJob, parameters);
     }
 
 }
