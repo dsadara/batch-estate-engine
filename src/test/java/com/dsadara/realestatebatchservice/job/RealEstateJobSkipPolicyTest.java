9e32ec850831be0e001eb00f05c60460f2e632dc
dsadara <chaehyun.forest@gmail.com>
2024-03-10 23:21:12 +0900

    test: step 예외 발생 시 ItemReader의 read() 호출 횟수 확인

---
+++ src/test/java/com/dsadara/realestatebatchservice/job/RealEstateJobSkipPolicyTest.java
@@ -0,0 +1,50 @@
+package com.dsadara.realestatebatchservice.job;
+
+
+import com.dsadara.realestatebatchservice.dto.RealEstateDto;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.mockito.Mockito;
+import org.springframework.batch.core.ExitStatus;
+import org.springframework.batch.core.JobExecution;
+import org.springframework.batch.core.JobParameters;
+import org.springframework.batch.core.JobParametersBuilder;
+import org.springframework.batch.item.ItemReader;
+import org.springframework.batch.test.JobLauncherTestUtils;
+import org.springframework.batch.test.context.SpringBatchTest;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+
+@SpringBootTest
+@SpringBatchTest
+public class RealEstateJobSkipPolicyTest {
+
+    @Autowired
+    private JobLauncherTestUtils jobLauncherTestUtils;
+
+    @MockBean(name = "createEmptyItemReader")
+    private ItemReader<RealEstateDto> mockItemReader;
+
+    @Test
+    public void testSkipPolicyInSlaveStep() throws Exception {
+        // given
+        Mockito.when(mockItemReader.read())
+                .thenReturn(new RealEstateDto(), new RealEstateDto(), null)
+                .thenThrow(new Exception("test Exception"));
+        JobParameters parameters = new JobParametersBuilder()
+                .addString("baseUrl", "http://SampleUrl1.co.kr")
+                .addString("serviceKey", "SampleServiceKey1")
+                .addString("bjdCode", "11110")
+                .addLong("time", System.currentTimeMillis())
+                .toJobParameters();
+
+        // when
+        JobExecution jobExecution = jobLauncherTestUtils.launchJob(parameters);
+
+        // then
+        Mockito.verify(mockItemReader, Mockito.atMost(300)).read();
+        Assertions.assertEquals(jobExecution.getExitStatus(), ExitStatus.FAILED);
+    }
+
+}
