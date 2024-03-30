aa7c0847501fe9bd8bcf73ae6491712989de514c
dsadara <chaehyun.forest@gmail.com>
2024-03-30 23:21:12 +0900

    refactor: 필요 없는 커스텀 Exception 제거

--- src/main/java/com/dsadara/realestatebatchservice/exception/SlaveStepFailureLimitExceededException.java
+++
@@ -1,7 +0,0 @@
-package com.dsadara.realestatebatchservice.exception;
-
-public class SlaveStepFailureLimitExceededException extends RuntimeException {
-    public SlaveStepFailureLimitExceededException() {
-        super("slave step의 실패 횟수가 50개를 초과하여 job의 실행을 중단합니다.");
-    }
-}
