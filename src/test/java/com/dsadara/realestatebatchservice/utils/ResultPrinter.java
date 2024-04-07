1fb2eae7dd72db711a829970a7c33fc179cd8a5d
dsadara <chaehyun.forest@gmail.com>
2024-04-07 16:38:39 +0900

    refactor: job 실행 결과 출력 메소드 두가지로 분리 refactor: 메소드명 변경 refactor: logger로 출력

--- src/test/java/com/dsadara/realestatebatchservice/utils/ResultPrinter.java
+++ src/test/java/com/dsadara/realestatebatchservice/utils/ResultPrinter.java
@@ -1,13 +1,15 @@
 package com.dsadara.realestatebatchservice.utils;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.batch.core.JobExecution;
 import org.springframework.batch.core.StepExecution;
 
 import java.util.ArrayList;
 import java.util.List;
 
+@Slf4j
 public class ResultPrinter {
-    public static void jobResultPrinter(JobExecution jobExecution) {
+    public static void printJobResultDetail(JobExecution jobExecution) {
         List<StepExecution> failedSteps = new ArrayList<>();
         List<StepExecution> completedSteps = new ArrayList<>();
         List<StepExecution> otherSteps = new ArrayList<>();
@@ -22,17 +24,38 @@ public class ResultPrinter {
             }
         }
 
+        log.info("[실패한 step 결과 출력 시작]");
         for (int i = 0; i < failedSteps.size(); i++) {
-            System.out.println("[" + (i + 1) + "]" + " 실패한 step: " + failedSteps.get(i).getStepName());
+            log.info("[" + (i + 1) + "]" + " 실패한 step: " + failedSteps.get(i).getStepName());
         }
-        System.out.println("=======>" + " 실패한 step 개수: " + failedSteps.size() + "\n");
+        log.info("[성공한 step 결과 출력 시작]");
         for (int i = 0; i < completedSteps.size(); i++) {
-            System.out.println("[" + (i + 1) + "]" + " 성공한 step: " + completedSteps.get(i).getStepName());
+            log.info("[" + (i + 1) + "]" + " 성공한 step: " + completedSteps.get(i).getStepName());
         }
-        System.out.println("=======>" + " 성공한 step 개수: " + completedSteps.size() + "\n");
+        log.info("[나머지 step 결과 출력 시작]");
         for (int i = 0; i < otherSteps.size(); i++) {
-            System.out.println("[" + (i + 1) + "]" + " 나머지 step: " + otherSteps.get(i).getStepName());
+            log.info("[" + (i + 1) + "]" + " 나머지 step: " + otherSteps.get(i).getStepName()
+                    + " -> " + "ExitStatus:" + otherSteps.get(i).getExitStatus().getExitCode());
         }
-        System.out.println("=======>" + " 나머지 step 개수: " + otherSteps.size() + "\n");
+    }
+
+    public static void printJobResult(JobExecution jobExecution) {
+        int failedStepCount = 0;
+        int completedStepCount = 0;
+        int otherStepCount = 0;
+
+        for (StepExecution stepExecution : jobExecution.getStepExecutions()) {
+            if (stepExecution.getExitStatus().getExitCode().equals("FAILED")) {
+                failedStepCount++;
+            } else if (stepExecution.getExitStatus().getExitCode().equals("COMPLETED")) {
+                completedStepCount++;
+            } else {
+                otherStepCount++;
+            }
+        }
+
+        log.info("실패한 step 개수 -> " + failedStepCount);
+        log.info("성공한 step 개수 -> " + completedStepCount);
+        log.info("나머지 step 개수 -> " + otherStepCount);
     }
 }
