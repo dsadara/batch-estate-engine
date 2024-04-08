bfcb7f4c3c86cb1829af7562782c01b202c52ac2
dsadara <chaehyun.forest@gmail.com>
2024-04-09 08:45:54 +0900

    feat: 부동산 데이터 5가지 모두 호출

--- src/main/java/com/dsadara/realestatebatchservice/runner/RealEstateJobLauncherCommandLineRunner.java
+++ src/main/java/com/dsadara/realestatebatchservice/runner/RealEstateJobLauncherCommandLineRunner.java
@@ -15,15 +15,79 @@ public class RealEstateJobLauncherCommandLineRunner implements CommandLineRunner
     @Autowired
     private GenerateApiQueryParam generateApiQueryParam;
     @Value("${openapi.request.url.aptRent}")
-    private String baseUrl;
+    private String baseUrl_aptRent;
+    @Value("${openapi.request.url.aptTrade}")
+    private String baseUrl_aptTrade;
+    @Value("${openapi.request.url.rowhouseRent}")
+    private String baseUrl_rowhouseRent;
+    @Value("${openapi.request.url.officetelRent}")
+    private String baseUrl_officetelRent;
+    @Value("${openapi.request.url.detachedhouseRent}")
+    private String baseUrl_detachedHouseRent;
     @Value("${openapi.request.serviceKey}")
     private String serviceKey;
 
     @Override
     public void run(String... args) throws Exception {
+        launchAptRentJob();
+        launchAptTradeJob();
+        launchRowHouseRentJob();
+        launchOfficetelRentJob();
+        launchDetachedHouseRent();
+    }
+
+    private void launchAptRentJob() throws Exception {
         for (String bjdCode : generateApiQueryParam.getBjdCodeList()) {
             JobParameters parameters = new JobParametersBuilder()
-                    .addString("baseUrl", baseUrl)
+                    .addString("baseUrl", baseUrl_aptRent)
+                    .addString("serviceKey", serviceKey)
+                    .addString("bjdCode", bjdCode)
+                    .addLong("time", System.currentTimeMillis())
+                    .toJobParameters();
+            realEstateJobLauncher.launchJob(parameters);
+        }
+    }
+
+    private void launchAptTradeJob() throws Exception {
+        for (String bjdCode : generateApiQueryParam.getBjdCodeList()) {
+            JobParameters parameters = new JobParametersBuilder()
+                    .addString("baseUrl", baseUrl_aptTrade)
+                    .addString("serviceKey", serviceKey)
+                    .addString("bjdCode", bjdCode)
+                    .addLong("time", System.currentTimeMillis())
+                    .toJobParameters();
+            realEstateJobLauncher.launchJob(parameters);
+        }
+    }
+
+    private void launchRowHouseRentJob() throws Exception {
+        for (String bjdCode : generateApiQueryParam.getBjdCodeList()) {
+            JobParameters parameters = new JobParametersBuilder()
+                    .addString("baseUrl", baseUrl_rowhouseRent)
+                    .addString("serviceKey", serviceKey)
+                    .addString("bjdCode", bjdCode)
+                    .addLong("time", System.currentTimeMillis())
+                    .toJobParameters();
+            realEstateJobLauncher.launchJob(parameters);
+        }
+    }
+
+    private void launchOfficetelRentJob() throws Exception {
+        for (String bjdCode : generateApiQueryParam.getBjdCodeList()) {
+            JobParameters parameters = new JobParametersBuilder()
+                    .addString("baseUrl", baseUrl_officetelRent)
+                    .addString("serviceKey", serviceKey)
+                    .addString("bjdCode", bjdCode)
+                    .addLong("time", System.currentTimeMillis())
+                    .toJobParameters();
+            realEstateJobLauncher.launchJob(parameters);
+        }
+    }
+
+    private void launchDetachedHouseRent() throws Exception {
+        for (String bjdCode : generateApiQueryParam.getBjdCodeList()) {
+            JobParameters parameters = new JobParametersBuilder()
+                    .addString("baseUrl", baseUrl_detachedHouseRent)
                     .addString("serviceKey", serviceKey)
                     .addString("bjdCode", bjdCode)
                     .addLong("time", System.currentTimeMillis())
