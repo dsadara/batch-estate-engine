42ed6015598950cbb95a4a126bb5b70405f2319b
dsadara <chaehyun.forest@gmail.com>
2024-04-09 18:11:24 +0900

    Revert "refactor: RealEstateJobLauncherCommandLineRunner bean으로 추가"
    This reverts commit b00cf0ffb5483d54ca5d1d051fde75487aa32381.

--- src/main/java/com/dsadara/realestatebatchservice/runner/RealEstateJobLauncherCommandLineRunner.java
+++ src/main/java/com/dsadara/realestatebatchservice/runner/RealEstateJobLauncherCommandLineRunner.java
@@ -7,9 +7,7 @@ import org.springframework.batch.core.JobParametersBuilder;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.CommandLineRunner;
-import org.springframework.stereotype.Component;
 
-@Component
 public class RealEstateJobLauncherCommandLineRunner implements CommandLineRunner {
 
     @Autowired
