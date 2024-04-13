d4f4855a795bf410aa212b1a78524da60d15a3a4
dsadara <chaehyun.forest@gmail.com>
2024-04-13 17:30:31 +0900

    test: 올바른 jobParameter가 추가되는지 확인

--- src/test/java/com/dsadara/realestatebatchservice/launcher/RealEstateJobLauncherRealTest.java
+++ src/test/java/com/dsadara/realestatebatchservice/launcher/RealEstateJobLauncherRealTest.java
@@ -2,23 +2,25 @@ package com.dsadara.realestatebatchservice.launcher;
 
 import com.dsadara.realestatebatchservice.service.GenerateApiQueryParam;
 import com.dsadara.realestatebatchservice.type.RealEstateType;
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
+import org.mockito.ArgumentCaptor;
+import org.mockito.Captor;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
+import org.springframework.batch.core.Job;
 import org.springframework.batch.core.JobParameters;
 import org.springframework.batch.core.launch.JobLauncher;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.core.env.Environment;
 
 import java.util.Arrays;
+import java.util.List;
 
 import static org.mockito.Mockito.*;
 
 @SpringBootTest
 class RealEstateJobLauncherRealTest {
-
-    @InjectMocks
-    private RealEstateJobLauncher realEstateJobLauncher;
 
     @Mock
     private GenerateApiQueryParam generateApiQueryParam;
@@ -26,9 +28,45 @@ class RealEstateJobLauncherRealTest {
     private Environment env;
     @Mock
     private JobLauncher jobLauncher;
+    @Mock
+    private Job realEstateJob;
+    @InjectMocks
+    private RealEstateJobLauncher realEstateJobLauncher;
+    @Captor
+    private ArgumentCaptor<JobParameters> jobParametersCaptor;
 
     @Test
-    void launchJob() throws Exception {
+    void testLaunchJobWithCorrectJobParameters() throws Exception {
+        // given
+        RealEstateType realEstateType = RealEstateType.APT_RENT;
+        List<String> bjdCodeList = Arrays.asList("11110", "11111");
+        String requestUrl = "https://api.example.com/data";
+        String serviceKey = "test-service-key";
+        when(env.getProperty("openapi.request.url." + realEstateType.name())).thenReturn(requestUrl);
+        when(env.getProperty("openapi.request.serviceKey")).thenReturn(serviceKey);
+        when(generateApiQueryParam.getBjdCodeList()).thenReturn(bjdCodeList);
+
+        // when
+        realEstateJobLauncher.launchJob(realEstateType);
+
+        // then
+        verify(jobLauncher, times(bjdCodeList.size())).run(eq(realEstateJob), jobParametersCaptor.capture());
+
+        List<JobParameters> capturedJobParametersList = jobParametersCaptor.getAllValues();
+        Assertions.assertEquals(2, capturedJobParametersList.size());
+
+        for (int i = 0; i < bjdCodeList.size(); i++) {
+            JobParameters params = capturedJobParametersList.get(i);
+            Assertions.assertEquals(requestUrl, params.getString("baseUrl"));
+            Assertions.assertEquals(serviceKey, params.getString("serviceKey"));
+            Assertions.assertEquals(bjdCodeList.get(i), params.getString("bjdCode"));
+            Assertions.assertEquals(realEstateType.name(), params.getString("realEstateType"));
+            Assertions.assertNotNull(params.getLong("time"));
+        }
+    }
+
+    @Test
+    void testLaunchJob() throws Exception {
         // given
         when(env.getProperty("openapi.request.url." + RealEstateType.APT_RENT.name())).thenReturn("http://example.com");
         when(env.getProperty("openapi.request.serviceKey")).thenReturn("serviceKey123");
