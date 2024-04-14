85787471499e1aecbf96433f2a11753c8539d76b
dsadara <chaehyun.forest@gmail.com>
2024-04-14 20:20:11 +0900

    feat: 정해진 횟수만큼 launchJob을 재시도하는 로직 추가

--- src/main/java/com/dsadara/realestatebatchservice/runner/RealEstateJobLauncherCommandLineRunner.java
+++ src/main/java/com/dsadara/realestatebatchservice/runner/RealEstateJobLauncherCommandLineRunner.java
@@ -2,18 +2,41 @@ package com.dsadara.realestatebatchservice.runner;
 
 import com.dsadara.realestatebatchservice.launcher.RealEstateJobLauncher;
 import com.dsadara.realestatebatchservice.type.RealEstateType;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 
+@Slf4j
 public class RealEstateJobLauncherCommandLineRunner implements CommandLineRunner {
 
     @Autowired
     private RealEstateJobLauncher realEstateJobLauncher;
 
     @Override
-    public void run(String... args) throws Exception {
+    public void run(String... args) {
+        launchJobsForAllTypes();
+    }
+
+    private void launchJobsForAllTypes() {
         for (RealEstateType realEstateType : RealEstateType.values()) {
-            realEstateJobLauncher.launchJob(realEstateType);
+            executeWithRetry(realEstateType, 2);
+        }
+    }
+
+    private void executeWithRetry(RealEstateType realEstateType, int maxAttempts) {
+        int attempt = 0;
+        while (attempt < maxAttempts) {
+            try {
+                realEstateJobLauncher.launchJob(realEstateType);
+                return;
+            } catch (Exception e) {
+                attempt++;
+                if (attempt >= maxAttempts) {
+                    log.error("[{}] {} 번 재시도 후에도 데이터 호출 실패.", realEstateType.getKrName(), maxAttempts);
+                } else {
+                    log.error("[{}] {} 번째 재실행 시작", realEstateType.getKrName(), attempt);
+                }
+            }
         }
     }
 }
