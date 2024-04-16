3370c47bfdc9c325fe9c508f056c6d97cfaa1d50
dsadara <chaehyun.forest@gmail.com>
2024-04-16 09:57:10 +0900

    fix: RealEstateJobLauncherTest로 클래스명 변경

--- src/test/java/com/dsadara/realestatebatchservice/launcher/RealEstateJobLauncherTest.java
+++ src/test/java/com/dsadara/realestatebatchservice/launcher/RealEstateJobLauncherTest.java
@@ -20,7 +20,7 @@ import java.util.List;
 import static org.mockito.Mockito.*;
 
 @SpringBootTest
-class RealEstateJobLauncherRealTest {
+class RealEstateJobLauncherTest {
 
     @Mock
     private GenerateApiQueryParam generateApiQueryParam;
