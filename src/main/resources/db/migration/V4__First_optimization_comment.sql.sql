4b0c391ed6bade78b0277ec9c6113369fd521aa5
dsadara <chaehyun.forest@gmail.com>
2024-06-05 14:43:22 +0900

    fix: 일단은 코멘트만 추가하기

--- src/main/resources/db/migration/V4__First_optimization_comment.sql.sql
+++ src/main/resources/db/migration/V4__First_optimization_comment.sql.sql
@@ -1,16 +1,15 @@
--- V3__First_optimization_column_data_type.sql
+-- V4__First_optimization_comment.sql
 
-# 코멘트 추가 및 제약조건 설정
+# 코멘트 추가
 ALTER TABLE batch_estate_engine.real_estate
-    MODIFY COLUMN id INT NOT NULL,
-    MODIFY COLUMN beop_jeong_dong VARCHAR(255) NOT NULL COMMENT '법정동',
+    MODIFY COLUMN beop_jeong_dong VARCHAR(255) COMMENT '법정동',
     MODIFY COLUMN request_renewal_right VARCHAR(255) COMMENT '갱신요구권사용',
     MODIFY COLUMN construct_year SMALLINT COMMENT '건축년도',
     MODIFY COLUMN contract_type VARCHAR(255) COMMENT '계약구분',
     MODIFY COLUMN contract_period VARCHAR(255) COMMENT '계약기간',
     MODIFY COLUMN contract_year SMALLINT COMMENT '계약년도',
-    MODIFY COLUMN contract_month SMALLINT NOT NULL COMMENT '계약월',
-    MODIFY COLUMN contract_day SMALLINT NOT NULL COMMENT '계약일',
+    MODIFY COLUMN contract_month SMALLINT COMMENT '계약월',
+    MODIFY COLUMN contract_day SMALLINT COMMENT '계약일',
     MODIFY COLUMN cancel_deal_day VARCHAR(255) COMMENT '해제사유 발생일',
     MODIFY COLUMN cancel_deal_type VARCHAR(255) COMMENT '해제여부',
     MODIFY COLUMN agent_address VARCHAR(255) COMMENT '중개사소재지',
@@ -19,12 +18,11 @@ ALTER TABLE batch_estate_engine.real_estate
     MODIFY COLUMN deposit DECIMAL COMMENT '보증금',
     MODIFY COLUMN deposit_before DECIMAL COMMENT '종전계약보증금',
     MODIFY COLUMN floor SMALLINT COMMENT '층',
-    MODIFY COLUMN jeon_yong_area VARCHAR(255) DEFAULT '0' COMMENT '전용면적, 계약면적',
+    MODIFY COLUMN jeon_yong_area VARCHAR(255) COMMENT '전용면적, 계약면적',
     MODIFY COLUMN monthly_rent DECIMAL COMMENT '월세',
     MODIFY COLUMN monthly_rent_before DECIMAL COMMENT '종전 계약 월세',
     MODIFY COLUMN name VARCHAR(255) COMMENT '이름',
     MODIFY COLUMN parcel_number VARCHAR(255) COMMENT '지번',
-    MODIFY COLUMN beop_jeong_dong_code VARCHAR(255) NOT NULL COMMENT '법정동 코드',
+    MODIFY COLUMN beop_jeong_dong_code VARCHAR(255) COMMENT '법정동 코드',
     MODIFY COLUMN si_gun_gu VARCHAR(255) COMMENT '시군구',
-    MODIFY COLUMN real_estate_type VARCHAR(255) NOT NULL COMMENT '부동산 종류',
-    MODIFY COLUMN created_at DATETIME(6) DEFAULT CURRENT_TIMESTAMP(6) NOT NULL;
+    MODIFY COLUMN real_estate_type VARCHAR(255) COMMENT '부동산 종류';
