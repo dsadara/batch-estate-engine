27036b12656d6baddef4376803f0c7c9fd57ca78
dsadara <chaehyun.forest@gmail.com>
2024-06-22 09:53:11 +0900

    feat: Rent, Sale 레포지토리 추가

---
+++ src/main/java/com/dsadara/realestatebatchservice/domain/SaleRepository.java
@@ -0,0 +1,8 @@
+package com.dsadara.realestatebatchservice.domain;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface SaleRepository extends JpaRepository<Sale, Integer> {
+}
