90879a43e9e89d472851042c6e18175c94ef12a5
dsadara <chaehyun.forest@gmail.com>
2024-06-28 21:22:15 +0900

    feat: RealEstateItemProcessor, RealEstateItemWriter 적용

--- src/main/java/com/dsadara/realestatebatchservice/config/RealEstateJobConfig.java
+++ src/main/java/com/dsadara/realestatebatchservice/config/RealEstateJobConfig.java
@@ -2,19 +2,16 @@ package com.dsadara.realestatebatchservice.config;
 
 import com.dsadara.realestatebatchservice.domain.RealEstate;
 import com.dsadara.realestatebatchservice.domain.RealEstateRepository;
-import com.dsadara.realestatebatchservice.domain.Rent;
 import com.dsadara.realestatebatchservice.domain.RentRepository;
-import com.dsadara.realestatebatchservice.domain.Sale;
 import com.dsadara.realestatebatchservice.domain.SaleRepository;
 import com.dsadara.realestatebatchservice.dto.RealEstateDto;
 import com.dsadara.realestatebatchservice.listener.SlaveStepFailureLimitListener;
 import com.dsadara.realestatebatchservice.listener.StepExceptionLogger;
 import com.dsadara.realestatebatchservice.processor.RealEstateItemProcessor;
-import com.dsadara.realestatebatchservice.processor.RentProcessor;
-import com.dsadara.realestatebatchservice.processor.SaleProcessor;
 import com.dsadara.realestatebatchservice.reader.ApiItemReader;
 import com.dsadara.realestatebatchservice.service.ApiRequester;
 import com.dsadara.realestatebatchservice.service.GenerateApiQueryParam;
+import com.dsadara.realestatebatchservice.writer.RealEstateItemWriter;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.batch.core.Job;
@@ -28,16 +25,11 @@ import org.springframework.batch.core.partition.support.Partitioner;
 import org.springframework.batch.item.ExecutionContext;
 import org.springframework.batch.item.ItemProcessor;
 import org.springframework.batch.item.ItemWriter;
-import org.springframework.batch.item.data.RepositoryItemWriter;
-import org.springframework.batch.item.data.builder.RepositoryItemWriterBuilder;
-import org.springframework.batch.item.support.CompositeItemProcessor;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Profile;
 
-import java.util.ArrayList;
-import java.util.Collections;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
@@ -81,74 +73,13 @@ public class RealEstateJobConfig {
     @JobScope
     public Step slaveStep() throws Exception {
         return stepBuilderFactory.get("계약월")
-                .<RealEstateDto, Object>chunk(100)
+                .<RealEstateDto, RealEstate>chunk(100)
                 .reader(createApiItemReader(null, null))
-                .processor(createCompositeItemProcessor(null))
-                .writer(createCompositeItemWriter())
+                .processor(createRealEstateItemProcessor(null))
+                .writer(createRealEstateItemWriter())
                 .listener(stepExceptionLogger)
                 .listener(slaveStepFailureLimitListener)
                 .build();
-    }
-
-    @Bean
-    @StepScope
-    public ApiItemReader createApiItemReader(
-            @Value("#{jobParameters['baseUrl']}") String baseUrl,
-            @Value("#{jobParameters['serviceKey']}") String serviceKey) throws Exception {
-        return new ApiItemReader(baseUrl, serviceKey, apiRequester);
-    }
-
-    @Bean
-    @StepScope
-    public CompositeItemProcessor<RealEstateDto, Object> createCompositeItemProcessor(@Value("#{jobParameters['realEstateType']}") String realRealEstateType) {
-        CompositeItemProcessor<RealEstateDto, Object> processor = new CompositeItemProcessor<>();
-
-        List<ItemProcessor<RealEstateDto, ?>> processors = new ArrayList<>();
-        processors.add(new RealEstateItemProcessor(realRealEstateType));
-        if (realRealEstateType.equals("아파트매매")) {     // 매매 데이터 처리
-            processors.add(new SaleProcessor());
-        } else {                                        // 전월세 데이터 처리
-            processors.add(new RentProcessor());
-        }
-
-        processor.setDelegates(processors);
-        return processor;
-    }
-
-    @Bean
-    public RepositoryItemWriter<RealEstate> createRealEstateWriter() {
-        return new RepositoryItemWriterBuilder<RealEstate>()
-                .repository(realEstateRepository)
-                .build();
-    }
-
-    @Bean
-    public RepositoryItemWriter<Rent> createRentWriter() {
-        return new RepositoryItemWriterBuilder<Rent>()
-                .repository(rentRepository)
-                .build();
-    }
-
-    @Bean
-    public RepositoryItemWriter<Sale> createSaleWriter() {
-        return new RepositoryItemWriterBuilder<Sale>()
-                .repository(saleRepository)
-                .build();
-    }
-
-    @Bean
-    public ItemWriter<Object> createCompositeItemWriter() {
-        return items -> {
-            for (Object item : items) {
-                if (item instanceof RealEstate) {
-                    createRealEstateWriter().write(Collections.singletonList((RealEstate) item));
-                } else if (item instanceof Rent) {
-                    createRentWriter().write(Collections.singletonList((Rent) item));
-                } else if (item instanceof Sale) {
-                    createSaleWriter().write(Collections.singletonList((Sale) item));
-                }
-            }
-        };
     }
 
     @Bean
@@ -163,6 +94,26 @@ public class RealEstateJobConfig {
             }
             return result;
         };
+    }
+
+    @Bean
+    @StepScope
+    public ApiItemReader createApiItemReader(
+            @Value("#{jobParameters['baseUrl']}") String baseUrl,
+            @Value("#{jobParameters['serviceKey']}") String serviceKey) throws Exception {
+        return new ApiItemReader(baseUrl, serviceKey, apiRequester);
+    }
+
+    @Bean
+    @StepScope
+    public ItemProcessor<RealEstateDto, RealEstate> createRealEstateItemProcessor(
+            @Value("#{jobParameters['realEstateType']}") String realEstateType) {
+        return new RealEstateItemProcessor(realEstateType);
+    }
+
+    @Bean
+    public ItemWriter<RealEstate> createRealEstateItemWriter() {
+        return new RealEstateItemWriter(realEstateRepository, rentRepository, saleRepository);
     }
 
 }
