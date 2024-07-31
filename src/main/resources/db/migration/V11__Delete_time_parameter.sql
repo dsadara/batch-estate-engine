c44e4dbe4f5e93a993af45c6121f1bee4f3b67e8
dsadara <chaehyun.forest@gmail.com>
2024-07-31 19:09:13 +0900

    chore: e5d7c789 이전으로 데이터베이스 롤백

--- src/main/resources/db/migration/V11__Delete_time_parameter.sql
+++
@@ -1,5 +0,0 @@
--- V11__Delete_time_parameter.sql
-
-DELETE
-FROM BATCH_JOB_EXECUTION_PARAMS
-WHERE KEY_NAME = 'time';
--- src/main/resources/db/migration/V12__Add_auto_increment_constraint.sql
+++
@@ -1,6 +0,0 @@
--- V1__add_auto_increment_to_real_estate.sql
-
--- Check if the id column already has AUTO_INCREMENT
--- If not, then modify the column to add AUTO_INCREMENT
-ALTER TABLE real_estate
-    MODIFY COLUMN id INT AUTO_INCREMENT;
